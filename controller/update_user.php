2729ea693480111a5ad252ea8667c3dba84b4c7e
Hakanasa <61895899+Hakanasa@users.noreply.github.com>
2020-03-07 15:42:47 +0700

    Profile semua

---
+++ controller/update_user.php
@@ -0,0 +1,24 @@
+<?php
+ if(isset($_POST['submit'])){
+     session_start();
+    $username   = $_SESSION['username'];
+    $n_depan = mysqli_real_escape_string($conn,$_POST['n_depan']);
+    $n_belakang = mysqli_real_escape_string($conn,$_POST['n_belakang']);
+    $password = mysqli_real_escape_string($conn,$_POST['password']);
+    $password = md5($password);
+    $deskripsi = mysqli_real_escape_string($conn,$_POST['deskripsi']);
+
+    $foto = $_FILES['foto']['name'];
+
+    $ket = move_uploaded_file($_FILES['foto']['tmp_name'], "gambar/".$_FILES['foto']['name']);
+
+    if($ket==1){
+        $update_sql = 'UPDATE User SET nama_depan="'.$n_depan.'",
+        nama_belakang="'.$n_belakang.'", password="'.$password.'",
+        gambar="'.$foto.'", profile_deskripsi="'.$deskripsi.'" WHERE username like "'.$username.'";';
+        mysqli_query($conn,$update_sql);
+    }else{
+        echo "Gagal";
+    }   
+}
+?>
--- view/profile.php
+++ view/profile.php
@@ -1,95 +1,168 @@
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <title>Bootstrap 4 Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.20/datatables.min.css"/>
-    
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.20/datatables.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-    <title>Kegiatan5</title>
-  </head>
+  
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  </style>
+</head>
+<body>
 
-  <body>
+<div class="container pt-3 pb-2">
+            <div class="row my-2">
+                <div class="col-lg-8 order-lg-2">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a href="" data-target="#profile" data-toggle="tab" class="nav-link active">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" data-target="#edit" data-toggle="tab" class="nav-link">Edit</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content py-4">
+                        <div class="tab-pane active" id="profile">
+                            <h5 class="mb-3">User Profile</h5>
+                            <hr>
+                            <div class="row">
+                                <div class="col-md-6">
+                                <?php
+                                session_start();
+                                $user_Sekarang = $_SESSION['username'];
+                                $sql = "SELECT * FROM user where username ='$user_Sekarang'";
+                                $result = $conn -> query($sql);
+                                $user=array();
 
+                                foreach($result as $row)
+                                array_push(
+                                    $user, 
+                                        new User(
+                                            $row['nama_depan'],
+                                            $row['nama_belakang'],
+                                            $row['username'],
+                                            $row['password'],
+                                            $row['gambar'],
+                                            $row['profile_deskripsi']
+                                        )
+                                        );
+                            ?>
 
-   <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">[IF635] Web Programming</a>
-    </div>
-    <ul class="nav navbar-nav navbar-right pr-1">
-      <li class="active"><a href="#">Products</a></li>
-    
-
-    </ul>
-  </div>
-</nav>
-<div class="container">
-    <form action="index.php" method="post">
-		    <div>
-      
-            <button type='submit' name='loc' value='data.php' class='btn btn-danger' style="float: right">Beranda</button> 
-		    </div>
-	  </form>
-
-</div>
-<br>
-<div class="container">
-profil
-<?php
-        session_start();
-        $user_Sekarang = $_SESSION['username'];
-        $sql = "SELECT * FROM user where username ='$user_Sekarang'";
-        $result = $conn -> query($sql);
-        $user=array();
-
-        foreach($result as $row)
-        array_push(
-            $user, 
-                new User(
-                    $row['nama_depan'],
-                    $row['nama_belakang'],
-                    $row['username'],
-                    $row['password'],
-                    $row['gambar'],
-                    $row['profile_deskripsi']
-                )
-        );
-        foreach($user as $row){
-        
-   
-            echo $row->getNama_depan();
-            echo $row->getNama_belakang();
-            echo $row->getUsername();
-            echo $row->getPassword();
-            echo $row->getGambar();
-            echo $row->getDeskripsi();
-
-            echo "<img src='gambar/".$row->getGambar()."' >";
-                         
-                       
-        
-
-        }
-    ?>
- 
-</div>
+                                    <h6>Nama Depan</h6>
+                                    <p>
+                                        <?php 
+                                        foreach($user as $row){
+                                          echo $row->getNama_depan(); 
+                                        }
+                                        ?>
+                                    </p>
+                                    <h6>Nama Belakang</h6>
+                                    <p>
+                                    <?php 
+                                        foreach($user as $row){
+                                          echo $row->getNama_belakang(); 
+                                        }
+                                        ?>
+                                    </p>
+                                    <h6>Deskripsi</h6>
+                                    <p>
+                                    <?php 
+                                        foreach($user as $row){
+                                          echo $row->getDeskripsi(); 
+                                        }
+                                        ?>
+                                    </p>
+                                </div>
+                            </div>
+                            <!--/row-->
+                        </div>
+                        <div class="tab-pane" id="edit">
+                            <form [formGroup]="updateForm" method="post" role="form" action='index.php' enctype="multipart/form-data">
+                            <div class="form-group row">
+                                    <label class="col-lg-3 col-form-label form-control-label">Username</label>
+                                    <div class="col-lg-9">
+                                        <input id="username" type="text" class="form-control" name="username" disabled value='
+                                        <?php 
+                                        foreach($user as $row){
+                                          echo $row->getUsername(); 
+                                        }; ?>'/>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-3 col-form-label form-control-label">Nama Depan</label>
+                                    <div class="col-lg-9">
+                                        <input id="n_depan" type="text" class="form-control" name="n_depan" value='
+                                        <?php 
+                                        foreach($user as $row){
+                                          echo $row->getNama_depan(); 
+                                        }; ?>'/>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-3 col-form-label form-control-label">Nama Belakang</label>
+                                    <div class="col-lg-9">
+                                        <input id="n_belakang" type="text" class="form-control" name="n_belakang"value='
+                                        <?php
+                                        foreach($user as $row){
+                                          echo $row->getNama_belakang(); 
+                                        }; ?>'/>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-3 col-form-label form-control-label">Password</label>
+                                    <div class="col-lg-9">
+                                        <input id="password" type="password" class="form-control" name="password">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-3 col-form-label form-control-label">Deskripsi</label>
+                                    <div class="col-lg-9">
+                                        <input id="deskripsi" type="text" class="form-control" name="deskripsi" value ='
+                                        <?php
+                                        foreach($user as $row){
+                                          echo $row->getDeskripsi(); 
+                                        }; ?>'/>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-lg-3 col-form-label form-control-label">Foto</label>
+                                    <div class="col-lg-9">
+                                    <input type="file" name="foto">
+                                    <br>
+                                    <br>
+                                    <input type='hidden' name='do' value='update_user.php'>  
+                                        <button type="submit" name="submit" value="data.php" class="btn btn-primary">Update</button>
+                                    <input type='hidden' name='loc' value='data.php'>
+                                        <button type="submit" class="btn btn-secondary" value="data.php">Cancel</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 order-lg-1 text-center">
+                <div class = "mx-auto img-fluid img-circle d-block" alt="avatar">
+                <?php
+                    foreach($user as $row){
+                        echo "<img width = 200 height = 200 src='gambar/".$row->getGambar()."'>";
+                    }
+                ?>
+                </div>
+                </div>
+            </div>
+        </div>
 </body>
 </html>
-
-
-
-
-
