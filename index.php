186a2b37be8ca796d08c674244aa2f982ebe3b16
Kleight <martinwongso234@gmail.com>
2020-03-07 11:03:30 +0700

    updAte tambash filke

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
-  
+
     if(isset($_POST['loc'])){
         $loc=$_POST['loc'];
     }else{
         $loc='login.php';
-    } 
-   
+    }
+
 
     include 'include/db_connect.php';
-   
-   
+
+
     include 'model/user.php';
     if(isset($_POST['do'])) include 'controller/' . $_POST['do'];
     include 'view/' . $loc;
-    include 'include/db_disconnect.php'; 
+    include 'include/db_disconnect.php';
+    u
 ?>
