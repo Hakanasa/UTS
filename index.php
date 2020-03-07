59161bf5a184666259216835a16886f6f2830560
dnina99 <ychrist54@gmail.com>
2020-03-07 13:13:14 +0700

    update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
 
     include 'model/user.php';
+    include 'model/timeline.php';
     if(isset($_POST['do'])) include 'controller/' . $_POST['do'];
     include 'view/' . $loc;
     include 'include/db_disconnect.php'; 
