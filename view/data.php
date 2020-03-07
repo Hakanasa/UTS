a7afd3256d5c5a3589f3836bff7d2fa8565b0974
Kleight <martinwongso234@gmail.com>
2020-03-07 16:09:02 +0700

    update data yang pro

--- view/data.php
+++ view/data.php
@@ -18,6 +18,10 @@
     right: 20px;
     display:none;
   }
+  img {
+  border-radius: 50%;
+  background: white;
+}
   </style>
   <script>
   $(document).ready(function(){
@@ -67,80 +71,103 @@
 
   <div class="container-fluid text-center">
     <div class="row content">
-      <div class="col-sm-2 sidenav bg-dark fixed-top mt-5 pt-3">
-        <p><a href="#">Link</a></p>
-        <p><a href="#">Link</a></p>
-        <p><a href="#">Link</a></p>
+      <div class="col-sm-2 sidenav fixed-top mt-5 pt-3">
+        <?php
+            ini_set('error_reporting', 0);
+            ini_set('display_errors', 0);
+            session_start();
+            $user_Sekarang = $_SESSION['username'];
+            $sql = "SELECT * FROM user where username ='$user_Sekarang'";
+            $result = $conn -> query($sql);
+            $user=array();
+
+            foreach($result as $row)
+            array_push(
+                $user,
+                    new User(
+                        $row['nama_depan'],
+                        $row['nama_belakang'],
+                        $row['username'],
+                        $row['password'],
+                        $row['gambar'],
+                        $row['profile_deskripsi']
+                    )
+            );
+            foreach($user as $row){
+                echo "<img class = 'round' width = '100px' height ='auto' src='gambar/".$row->getGambar()."' >";
+                echo "<br>Hai apa kabar ".$row->getNama_depan();
+            }
+        ?>
       </div>
-      <div class="col-sm-2 sidenav bg-dark">
+      <div class="col-sm-2">
       </div>
-      <div class="col-sm-8 text-left">
+      <div class="col-sm-6 text-left">
         <div class="container" style="margin-top:30px">
       <!-- DropDown Start-->
-      <p>
-    <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-      New Post
-    </a>
-  </p>
-  <div class="collapse" id="collapseExample">
-    <div class="tab-pane" id="edit">
-        <form [formGroup]="updateForm" role="form">
-            <div class="form-group row">
-                <label class="col-lg-3 col-form-label form-control-label">Nama Lengkap</label>
-                <div class="col-lg-9">
-                    <input id="nama_lengkap" type="text" class="form-control" formControlName="nama_lengkap" [ngClass]="{ 'is-invalid': submitted && f.nama_lengkap.errors }"/>
-                    <div *ngIf="submitted && f.nama_lengkap.errors" class="invalid-feedback">
-                        <div *ngIf="f.nama_lengkap.errors.required">Nama Lengkap is required</div>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-lg-3 col-form-label form-control-label">Tanggal Lahir</label>
-                <div class="col-lg-9">
-                    <input id="tanggal_lahir" type="date" class="form-control" formControlName="tanggal_lahir" [ngClass]="{ 'is-invalid': submitted && f.tanggal_lahir.errors }"/>
-                    <div *ngIf="submitted && f.tanggal_lahir.errors" class="invalid-feedback">
-                        <div *ngIf="f.tanggal_lahir.errors.required">Tanggal Lahir is required</div>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-lg-3 col-form-label form-control-label">Alamat</label>
-                <div class="col-lg-9">
-                    <input id="alamat" type="text" class="form-control" formControlName="alamat" [ngClass]="{ 'is-invalid': submitted && f.alamat.errors }"/>
-                    <div *ngIf="submitted && f.alamat.errors" class="invalid-feedback">
-                        <div *ngIf="f.alamat.errors.required">Alamat is required</div>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-lg-3 col-form-label form-control-label">Foto</label>
-                <div class="col-lg-9">
-                    <input id="foto" type="text" class="form-control" formControlName="foto" [ngClass]="{ 'is-invalid': submitted && f.foto.errors }"/>
-                    <div *ngIf="submitted && f.foto.errors" class="invalid-feedback">
-                        <div *ngIf="f.foto.errors.required">Foto is required</div>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-lg-3 col-form-label form-control-label">Password</label>
-                <div class="col-lg-9">
-                    <input id="password" type="password" class="form-control" formControlName="password" [ngClass]="{ 'is-invalid': submitted && f.password.errors }"/>
-                    <div *ngIf="submitted && f.password.errors" class="invalid-feedback">
-                        <div *ngIf="f.password.errors.required">Password is required</div>
-                    </div>
-                </div>
-            </div>
-            <br/>
-            <div class="form-group row">
-                <label class="col-lg-3 col-form-label form-control-label"></label>
-                <div class="col-lg-9">
-                    <input type="submit" class="btn btn-primary" (click)="update()" value="Save Changes">
-                    <input type="reset" class="btn btn-secondary" value="Cancel" routerLink="/home">
-                </div>
-            </div>
-        </form>
-    </div>
-  </div>
+        <p>
+          <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            New Post
+          </a>
+        </p>
+        <div class="collapse" id="collapseExample">
+          <div class="tab-pane" id="edit">
+              <form [formGroup]="updateForm" role="form">
+                  <div class="form-group row">
+                      <label class="col-lg-3 col-form-label form-control-label">Nama Lengkap</label>
+                      <div class="col-lg-9">
+                          <input id="nama_lengkap" type="text" class="form-control" formControlName="nama_lengkap" [ngClass]="{ 'is-invalid': submitted && f.nama_lengkap.errors }"/>
+                          <div *ngIf="submitted && f.nama_lengkap.errors" class="invalid-feedback">
+                              <div *ngIf="f.nama_lengkap.errors.required">Nama Lengkap is required</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-lg-3 col-form-label form-control-label">Tanggal Lahir</label>
+                      <div class="col-lg-9">
+                          <input id="tanggal_lahir" type="date" class="form-control" formControlName="tanggal_lahir" [ngClass]="{ 'is-invalid': submitted && f.tanggal_lahir.errors }"/>
+                          <div *ngIf="submitted && f.tanggal_lahir.errors" class="invalid-feedback">
+                              <div *ngIf="f.tanggal_lahir.errors.required">Tanggal Lahir is required</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-lg-3 col-form-label form-control-label">Alamat</label>
+                      <div class="col-lg-9">
+                          <input id="alamat" type="text" class="form-control" formControlName="alamat" [ngClass]="{ 'is-invalid': submitted && f.alamat.errors }"/>
+                          <div *ngIf="submitted && f.alamat.errors" class="invalid-feedback">
+                              <div *ngIf="f.alamat.errors.required">Alamat is required</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-lg-3 col-form-label form-control-label">Foto</label>
+                      <div class="col-lg-9">
+                          <input id="foto" type="text" class="form-control" formControlName="foto" [ngClass]="{ 'is-invalid': submitted && f.foto.errors }"/>
+                          <div *ngIf="submitted && f.foto.errors" class="invalid-feedback">
+                              <div *ngIf="f.foto.errors.required">Foto is required</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label class="col-lg-3 col-form-label form-control-label">Password</label>
+                      <div class="col-lg-9">
+                          <input id="password" type="password" class="form-control" formControlName="password" [ngClass]="{ 'is-invalid': submitted && f.password.errors }"/>
+                          <div *ngIf="submitted && f.password.errors" class="invalid-feedback">
+                              <div *ngIf="f.password.errors.required">Password is required</div>
+                          </div>
+                      </div>
+                  </div>
+                  <br/>
+                  <div class="form-group row">
+                      <label class="col-lg-3 col-form-label form-control-label"></label>
+                      <div class="col-lg-9">
+                          <input type="submit" class="btn btn-primary" (click)="update()" value="Save Changes">
+                          <input type="reset" class="btn btn-secondary" value="Cancel" routerLink="/home">
+                      </div>
+                  </div>
+              </form>
+          </div>
+        </div>
       <!-- DropDown End-->
       profil
           <form action = "index.php" method="post">
@@ -150,56 +177,16 @@
             <input type='hidden' name='loc' value='data.php'>
           </form>
           <?php
-              session_start();
-              $user_Sekarang = $_SESSION['username'];
-              $sql = "SELECT * FROM user where username ='$user_Sekarang'";
-              $result = $conn -> query($sql);
-              $user=array();
-
-              foreach($result as $row)
-              array_push(
-                  $user,
-                      new User(
-                          $row['nama_depan'],
-                          $row['nama_belakang'],
-                          $row['username'],
-                          $row['password'],
-                          $row['gambar'],
-                          $row['profile_deskripsi']
-                      )
-              );
               foreach($user as $row){
-
-
-                  echo $row->getNama_depan();
-                  echo $row->getNama_belakang();
-                  echo $row->getUsername();
-                  echo $row->getPassword();
-                  echo $row->getGambar();
-                  echo $row->getDeskripsi();
-
-                  echo "<img src='gambar/".$row->getGambar()."' >";
-
-
-
-
+                  echo "<img width = '500px' height = '500px' src='gambar/".$row->getGambar()."' >";
               }
           ?>
-      </div>
-      <div class="col-sm-2 mr-0" style="right: 0px;">
-        <div class="well">
-          <p>ADS</p>
-        </div>
-        <div class="well">
-          <p>ADS</p>
         </div>
       </div>
     </div>
   </div>
-
   <a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
-
-
+</div>
 
 </body>
 </html>
