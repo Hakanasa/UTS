857c527a3f6c3f94cdf265a362ba2f3ad6709b59
jessvaleria72 <jessvaleria72@users.noreply.github.com>
2020-03-09 18:20:22 +0700

    add friends

--- view/list_users.php
+++ view/list_users.php
@@ -20,10 +20,15 @@
             )
     );
     foreach($user as $row){
+        echo '<form method="post" action="index.php">';
         echo "<img class = 'round' width = '100px' height ='100px' src='gambar/".$row->getGambar()."' ><br>";
         echo $row->getNama_depan()."<br>";
         echo $row->getNama_belakang()."<br>";
         echo $row->getDeskripsi()."<br>";
-        echo "<button name='submit' value='.php' class='btn btn-dark'>Add to Friend</button><br><br>";
+        echo "<input type='hidden' name='user' value='".$row->getUsername()."'>";
+        echo "<input type='hidden' name='do' value='add_friends.php'>"; 
+        echo "<button name='submit' value='add_friends.php' class='btn btn-dark'>Add to Friend</button><br><br>";
+        echo "<input type='hidden' name='loc' value='data.php'>"; 
+        echo '</form>';
     }
 ?>
