091abaf74953555f55c1c6bf55010fb38d637fb4
Kleight <martinwongso234@gmail.com>
2020-03-09 14:24:48 +0700

    Komentar update

--- controller/komentar.php
+++ controller/komentar.php
@@ -2,12 +2,12 @@
 <?php
  if(isset($_POST['submit'])){
     session_start();
-        $id = mysqli_real_escape_string($conn,$_POST['id']);
+        $id = mysqli_real_escape_string($conn,$_POST['Post_id']);
         $komentar = mysqli_real_escape_string($conn,$_POST['komentar']);
         $time = date('Y-m-d H:i:s');
         $commenter = $_SESSION['username'];
 
-   
+
         $insert_sql_komen = "INSERT INTO comment (post_id, commenter, description, time) VALUES ('$id','$commenter','$komentar','$time')";
 
         mysqli_query($conn,$insert_sql_komen);
