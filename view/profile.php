2618557834e337b321eee665c9f3f83f88ac695a
Kleight <martinwongso234@gmail.com>
2020-03-09 13:54:52 +0700

    Update Profile.php

--- view/profile.php
+++ view/profile.php
@@ -1,27 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap 4 Website Example</title>
+  <title>Profile</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
-  
-  <style>
-  .fakeimg {
-    height: 200px;
-    background: #aaa;
-  }
-  </style>
 </head>
 <body>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <a class="navbar-brand" name="do" value="data.php"href="index.php">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex flex-row-reverse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <form action="index.php" method="post" class="px-2">
+            <div>
+                <input type='hidden' name='do' value='logout.php'>
+                <button type='submit' name='loc' value='login.php' class='btn btn-secondary' style="float: right">Logout</button>
+            </div>
+        </form>
+        <form action="index.php" method="post" class="px-2">
+            <div>
+                <button type='submit' name='loc' value='profile.php' class='btn btn-secondary' style="float: right">Profile</button>
+            </div>
+        </form>
+      </ul>
+    </div>
+  </nav>
 
 <div class="container pt-3 pb-2">
             <div class="row my-2">
@@ -49,7 +58,7 @@
 
                                 foreach($result as $row)
                                 array_push(
-                                    $user, 
+                                    $user,
                                         new User(
                                             $row['nama_depan'],
                                             $row['nama_belakang'],
@@ -63,25 +72,25 @@
 
                                     <h6>Nama Depan</h6>
                                     <p>
-                                        <?php 
+                                        <?php
                                         foreach($user as $row){
-                                          echo $row->getNama_depan(); 
+                                          echo $row->getNama_depan();
                                         }
                                         ?>
                                     </p>
                                     <h6>Nama Belakang</h6>
                                     <p>
-                                    <?php 
+                                    <?php
                                         foreach($user as $row){
-                                          echo $row->getNama_belakang(); 
+                                          echo $row->getNama_belakang();
                                         }
                                         ?>
                                     </p>
                                     <h6>Deskripsi</h6>
                                     <p>
-                                    <?php 
+                                    <?php
                                         foreach($user as $row){
-                                          echo $row->getDeskripsi(); 
+                                          echo $row->getDeskripsi();
                                         }
                                         ?>
                                     </p>
@@ -94,30 +103,27 @@
                             <div class="form-group row">
                                     <label class="col-lg-3 col-form-label form-control-label">Username</label>
                                     <div class="col-lg-9">
-                                        <input id="username" type="text" class="form-control" name="username" disabled value='
-                                        <?php 
+                                        <input id="username" type="text" class="form-control" name="username" disabled value='<?php
                                         foreach($user as $row){
-                                          echo $row->getUsername(); 
+                                          echo $row->getUsername();
                                         }; ?>'/>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label form-control-label">Nama Depan</label>
                                     <div class="col-lg-9">
-                                        <input id="n_depan" type="text" class="form-control" name="n_depan" value='
-                                        <?php 
+                                        <input id="n_depan" type="text" class="form-control" name="n_depan" value='<?php
                                         foreach($user as $row){
-                                          echo $row->getNama_depan(); 
+                                          echo $row->getNama_depan();
                                         }; ?>'/>
                                     </div>
                                 </div>
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label form-control-label">Nama Belakang</label>
                                     <div class="col-lg-9">
-                                        <input id="n_belakang" type="text" class="form-control" name="n_belakang"value='
-                                        <?php
+                                        <input id="n_belakang" type="text" class="form-control" name="n_belakang"value='<?php
                                         foreach($user as $row){
-                                          echo $row->getNama_belakang(); 
+                                          echo $row->getNama_belakang();
                                         }; ?>'/>
                                     </div>
                                 </div>
@@ -130,10 +136,9 @@
                                 <div class="form-group row">
                                     <label class="col-lg-3 col-form-label form-control-label">Deskripsi</label>
                                     <div class="col-lg-9">
-                                        <input id="deskripsi" type="text" class="form-control" name="deskripsi" value ='
-                                        <?php
+                                        <input id="deskripsi" type="text" class="form-control" name="deskripsi" value ='<?php
                                         foreach($user as $row){
-                                          echo $row->getDeskripsi(); 
+                                          echo $row->getDeskripsi();
                                         }; ?>'/>
                                     </div>
                                 </div>
@@ -143,7 +148,7 @@
                                     <input type="file" name="foto">
                                     <br>
                                     <br>
-                                    <input type='hidden' name='do' value='update_user.php'>  
+                                    <input type='hidden' name='do' value='update_user.php'>
                                         <button type="submit" name="submit" value="data.php" class="btn btn-primary">Update</button>
                                     <input type='hidden' name='loc' value='data.php'>
                                         <button type="submit" class="btn btn-secondary" value="data.php">Cancel</button>
