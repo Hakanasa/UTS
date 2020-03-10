7b0de1de3faa748245e117c9a5603da7c924a876
Kleight <martinwongso234@gmail.com>
2020-03-10 12:41:23 +0700

    udpate assset list_user login

--- view/list_users.php
+++ view/list_users.php
@@ -1,34 +1,128 @@
-<?php
-    ini_set('error_reporting', 0);
-    ini_set('display_errors', 0);
-    session_start();
-    $user_Sekarang = $_SESSION['username'];
-    $sql = "SELECT * FROM user where username !='$user_Sekarang'";
-    $result = $conn -> query($sql);
-    $user=array();
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>User</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <style>
+    body{
+    background:#FAFAFA;
+}
+.people-nearby .google-maps{
+  background: #f8f8f8;
+  border-radius: 4px;
+  border: 1px solid #f1f2f2;
+  padding: 20px;
+  margin-bottom: 20px;
+}
 
-    foreach($result as $row)
-    array_push(
-        $user,
-            new User(
-                $row['nama_depan'],
-                $row['nama_belakang'],
-                $row['username'],
-                $row['password'],
-                $row['gambar'],
-                $row['profile_deskripsi']
-            )
-    );
-    foreach($user as $row){
-        echo '<form method="post" action="index.php">';
-        echo "<img class = 'round' width = '100px' height ='100px' src='gambar/".$row->getGambar()."' ><br>";
-        echo $row->getNama_depan()."<br>";
-        echo $row->getNama_belakang()."<br>";
-        echo $row->getDeskripsi()."<br>";
-        echo "<input type='hidden' name='user' value='".$row->getUsername()."'>";
-        echo "<input type='hidden' name='do' value='add_friends.php'>"; 
-        echo "<button name='submit' value='add_friends.php' class='btn btn-dark'>Add to Friend</button><br><br>";
-        echo "<input type='hidden' name='loc' value='data.php'>"; 
-        echo '</form>';
-    }
-?>
+.people-nearby .google-maps .map{
+  height: 300px;
+  width: 100%;
+  border: none;
+}
+
+.people-nearby .nearby-user{
+  padding: 20px 0;
+  border-top: 1px solid #f1f2f2;
+  border-bottom: 1px solid #f1f2f2;
+  margin-bottom: 20px;
+}
+
+img.profile-photo-lg{
+  height: 80px;
+  width: 80px;
+  border-radius: 50%;
+}
+
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+      <a class="navbar-brand" name="do" value="data.php"href="index.php">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex flex-row-reverse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <form action="index.php" method="post" class="px-2">
+              <div>
+                  <input type='hidden' name='do' value='logout.php'>
+                  <button type='submit' name='loc' value='login.php' class='btn btn-secondary' style="float: right">Logout</button>
+              </div>
+          </form>
+          <form action="index.php" method="post" class="px-2">
+              <div>
+                  <button type='submit' name='loc' value='profile.php' class='btn btn-secondary' style="float: right">Profile</button>
+              </div>
+          </form>
+        </ul>
+      </div>
+    </nav>
+    <?php
+        ini_set('error_reporting', 0);
+        ini_set('display_errors', 0);
+        session_start();
+        $user_Sekarang = $_SESSION['username'];
+        $sql = "SELECT * FROM user where username !='$user_Sekarang'";
+        $result = $conn -> query($sql);
+        $user=array();
+
+        foreach($result as $row)
+        array_push(
+            $user,
+                new User(
+                    $row['nama_depan'],
+                    $row['nama_belakang'],
+                    $row['username'],
+                    $row['password'],
+                    $row['gambar'],
+                    $row['profile_deskripsi']
+                )
+        );
+
+          echo '<div class="container">';
+          echo '<div class="row">';
+                echo'<div class="col-md-8">';
+                    echo'<div class="people-nearby">';
+        foreach($user as $row){
+          echo 'MASIH BELUM SELESAI';
+          echo '<form method="post" action="index.php">';
+          echo'<div class="nearby-user">';
+          echo'<div class="row">';
+          echo'<div class="col-md-2 col-sm-2">';
+          echo'<img src="/gambar/'.$row->getGambar().'" alt="user" class="profile-photo-lg">';
+          echo'</div>';
+          echo'<div class="col-md-7 col-sm-7">';
+          echo'<h5><a href="#" class="profile-link text-dark">'.$row->getNama_depan().' '.$row->getNama_belakang().'</a></h5>';
+          echo'<p>'.$row->getDeskripsi().'</p>';
+          echo'<p class="text-muted">500m away</p>';
+          echo'</div>';
+          echo'<div class="col-md-3 col-sm-3">';
+          echo'<button class="btn btn-dark pull-right">Add Friend</button>';
+          echo'</div>';
+          echo'</div>';
+          echo'</div>';
+          echo '</form>';
+            // echo '<form method="post" action="index.php">';
+            // echo "<img class = 'round' width = '100px' height ='100px' src='gambar/".$row->getGambar()."' ><br>";
+            // echo $row->getNama_depan()."<br>";
+            // echo $row->getNama_belakang()."<br>";
+            // echo $row->getDeskripsi()."<br>";
+            // echo "<input type='hidden' name='user' value='".$row->getUsername()."'>";
+            // echo "<input type='hidden' name='do' value='add_friends.php'>";
+            // echo "<button name='submit' value='add_friends.php' class='btn btn-dark'>Add to Friend</button><br><br>";
+            // echo "<input type='hidden' name='loc' value='data.php'>";
+            // echo '</form>';
+        }
+      echo'</div>';
+      echo'</div>';
+      echo'</div>';
+      echo'</div>';
+    ?>
+  </body>
+</html>
--- view/login.php
+++ view/login.php
@@ -10,24 +10,27 @@
         <link rel="stylesheet" type="text/css" href="assets/examples.css" />
 
         <style>
-        .bg0 {
-          background-image: './assets/BG-0.jpg';
+        #section0{
+          background-image: url(assets/BG-2.jpg);
+        }
+        #section2{
+          background-image: url(assets/BG-1.jpg);
         }
         </style>
     </head>
     <body>
       <div id="fullpage">
     <div class="section active" id="section0">
-        <div class="slide" id="slide0" active>
+        <div class="slide active" id="slide0" active>
             <div class="intro">
-                <h1>WELCOME</h1>
+                <h1 class="text-white">WELCOME</h1>
             </div>
         </div>
         <div class="slide" id="slide1">
-            <h1>Scroll Down for Sign In</h1>
+            <h1 class="text-white">Scroll Down for Sign In</h1>
         </div>
     </div>
-    <div class="section" id="section1">
+    <div class="section bg-white" id="section2">
       <div class="row">
         <aside class="col-sm-4">
         </aside>
@@ -59,8 +62,6 @@
               <input type='hidden' name='loc' value='data.php'>
               <br>
               <button type='submit' name='loc' value='register_user.php' class='btn btn-dark btn-block'>Register</button>
-
-
           </div>
         </form>
       </article>
@@ -73,61 +74,11 @@
 <script type="text/javascript" src="assets/examples.js"></script>
 <script type="text/javascript">
     var myFullpage = new fullpage('#fullpage', {
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
         menu: '#menu',
         lazyLoad: true
     });
 </script>
-
-	<!-- <div class="row">
-		<aside class="col-sm-4">
-
-		<div class="card">
-
-
-	</div>
-	</aside>
-
-	<aside class="col-sm-4">
-
-	<div class="card">
-	<article class="card-body">
-	<h4 class="card-title text-center mb-4 mt-1">Sign in</h4>
-		<form action="index.php" method="post" onSubmit="return validasi()">
-			<div class="form-group">
-				<div class="input-group">
-					<div class="input-group-prepend">
-						<span class="input-group-text"> <i class="fa fa-user"></i> </span>
-					</div>
-						<input class="form-control" type="text" name="username" id="username" placeholder="Masukan Username" autofocus>
-				</div>
-			</div>
-
-			<div class="form-group">
-				<div class="input-group">
-					<div class="input-group-prepend">
-						<span class="input-group-text"> <i class="fa fa-key"></i> </span>
-					</div>
-
-					<input class="form-control" type="password" name="password" id="password" placeholder="Masukan Password">
-				</div>
-			</div>
-			<div class="form-group">
-
-                	<input type='hidden' name='do' value='check_loginuser.php'>
-                	<button type='submit' name='submit' value='data.php' class='btn btn-dark btn-block'>Login</button>
-					<input type='hidden' name='loc' value='data.php'>
-					<br>
-					<button type='submit' name='loc' value='register_user.php' class='btn btn-dark btn-block'>Register</button>
-
-
-			</div>
-		</form>
-	</article>
-	</div>
-	</aside>
-	</div> -->
 </body>
 
 
