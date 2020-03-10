f2b59f6dc17cfe7b33a313f7534e91c37400c563
Kleight <martinwongso234@gmail.com>
2020-03-10 20:37:48 +0700

    login

--- view/login.php
+++ view/login.php
@@ -11,7 +11,7 @@
 
         <style>
         #section2{
-          background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+          background: linear-gradient(124deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);
         	background-size: 400% 400%;
         	animation: gradient 15s ease infinite;
         }
